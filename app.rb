97e3315b25e9d70a2bc0145d94c11739ea3236b7
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-09-24 19:54:46 -0700

    remove pry before being deployed to heroku

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**/*.rb')
 require('./lib/word')
-require('pry')
 
 get('/') do
   @list = Word.all()
--- lib/word.rb
+++ lib/word.rb
@@ -7,18 +7,21 @@ class Word
  def initialize(vocabulary_word, definition)
    @vocabulary_word = vocabulary_word
    @definition = definition
-  @id = @@list.length + 1
+   @id = @@list.length + 1
  end
 
  def self.all()
    @@list
  end
+
  def save ()
    @@list.push(self)
  end
+
  def self.clear()
    @@list = []
  end
+
  def self.find(id)
    word_id = id.to_i
    @@list.each do |word|
